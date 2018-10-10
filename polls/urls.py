eb94a891fbb14d255ab922b0edbbe8c827d65aba
Marco Seguri <seguri@users.noreply.github.com>
2018-10-10 20:07:22 +0200

    Part 4

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # e.g.: /polls/
-    path('', views.index, name='index'),
-    # e.g.: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # e.g.: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # e.g.: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
